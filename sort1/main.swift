7eb66c58f06d2ed9e0667fb10248e29e51363175
王蕾 <867550109@qq.com>
2018-09-29 09:26:40 +0800

    change
    加入自己的代码

--- sort1/main.swift
+++ sort1/main.swift
@@ -7,6 +7,23 @@
 //
 
 import Foundation
-
-print("Hello, World!")
-
+var i=0
+var j=0
+var temp=0
+var num:[Int]=[0,2,9,3,6,8,4,5,7,6,1]
+for i in 0...8
+{
+    for j in 0...(8-i)
+    {
+        if num[j+1]>num[j]
+        {
+            temp=num[j]
+            num[j]=num[j+1]
+            num[j+1]=temp
+        }
+    }
+}
+for i in 0...9
+{
+    print(num[i])
+}
